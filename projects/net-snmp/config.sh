9ce53f5278cd525d600c6484b09ca93c476f9485
Ronny Trommer <ronny@opennms.org>
2018-11-14 09:43:25 +0100

    Migrate Net-SNMP to Alpine

--- projects/net-snmp/config.sh
+++ projects/net-snmp/config.sh
@@ -3,11 +3,11 @@
 # shellcheck disable=SC2034
 
 # Base Image Dependency
-BASE_IMAGE="no42org/centos"
-BASE_IMAGE_VERSION="7.5.1804-b1"
+BASE_IMAGE="alpine"
+BASE_IMAGE_VERSION="3.8"
 
 # Version information
-VERSION="5.7.2"
+VERSION="5.7.3-r11"
 BUILD_NUMBER="b1"
 IMAGE_VERSION="${VERSION}-${BUILD_NUMBER}"
 
