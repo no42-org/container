48d2a1c18b50acfb16e35e74cdf079f4bbed6422
Ronny Trommer <ronny@opennms.org>
2018-11-16 08:31:24 +0100

    Revert "Move Horizon and Minion in source repository"
    This reverts commit bcd6024111fb7171f196e519a3a10fe00191c3bf.

---
+++ projects/minion/build_container_image.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+set -x
+
+# shellcheck source=projects/minion/config.sh
+source ./config.sh
+
+docker build -t "${CONTAINER_PROJECT}:${IMAGE_VERSION}" \
+  --build-arg BASE_IMAGE="${BASE_IMAGE}" \
+  --build-arg BASE_IMAGE_VERSION="${BASE_IMAGE_VERSION}" \
+  --build-arg LOCAL_RPMS="${LOCAL_RPMS}" \
+  --build-arg REPO_RELEASE="${REPO_RELEASE}" \
+  --build-arg VERSION="${VERSION}" \
+  --build-arg REPO_RPM_URL="${REPO_RPM_URL}" \
+  --build-arg REPO_KEY_URL="${REPO_KEY_URL}" \
+  --build-arg REPO_URL="${REPO_URL}" \
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
+  --build-arg OPENNMS_PACKAGES="${PACKAGES}" \
+  --build-arg OPENNMS_PACKAGES="${OPENNMS_PACKAGES}" \
+  --build-arg USER="${USER}" \
+  --build-arg GROUP="${GROUP}" \
+  .
+
+docker image save "${CONTAINER_PROJECT}:${IMAGE_VERSION}" -o "${CONTAINER_IMAGE}"
