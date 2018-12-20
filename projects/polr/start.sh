4fc6b7eddb2a33a7bb5a5b34fd64949aa91a286a
Ronny Trommer <ronny@opennms.org>
2018-12-20 13:11:47 +0100

    Fix shellcheck issues

--- projects/polr/start.sh
+++ projects/polr/start.sh
@@ -4,7 +4,7 @@ if [ ! -f ".env" ]; then
     POLR_GENERATED_AT=$(date +"%B %d, %Y")
     export POLR_GENERATED_AT
 
-    APP_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    APP_KEY=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 32 | head -n 1)
     export APP_KEY
 
     envsubst < ".env_polr" > ".env"
