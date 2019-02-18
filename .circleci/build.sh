028df1714a99085b54776aad10fa008668b7fcb2
Ronny Trommer <ronny@opennms.org>
2019-02-18 13:09:22 +0100

    Fix

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -6,8 +6,8 @@ echo "Detect changes in projects"
 if git diff --name-only HEAD^...HEAD | grep "^projects/${PROJECT}"; then
   echo "Changes here, run the build"
   ./build_container_image.sh
-  ~/opennms-container/.circleci/tag.sh
-  ~/opennms-container/.circleci/publish.sh
+  ~/my-container/.circleci/tag.sh
+  ~/my-container/.circleci/publish.sh
 else
   echo "No changes detected"
   exit 0
