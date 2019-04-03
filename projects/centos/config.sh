a9192b60ef3fcbd9decd9ef3e62f809c77c70f7b
Ronny Trommer <ronny@opennms.org>
2019-04-03 15:28:33 +0200

    Fix includes

--- projects/centos/config.sh
+++ projects/centos/config.sh
@@ -2,16 +2,19 @@
 
 # shellcheck disable=SC2034
 
+set -x
+
 # Configure base image dependency
 BASE_IMAGE="centos"
 BASE_IMAGE_VERSION="7.6.1810"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 BUILD_NUMBER="b5"
+
 # Floating tags
 IMAGE_VERSION=("${BASE_IMAGE_VERSION}-${BUILD_NUMBER}"
                "${BASE_IMAGE_VERSION}")
 
 # Most specific tag when it is not build locally and in CircleCI
 if [ -n "${CIRCLE_BUILD_NUM}" ]; then
-  IMAGE_VERSION+=("${BASE_IMAGE_VERSION}-${BUILD_NUMBER}.${CIRCLE_BUILD_NUM}")
+  IMAGE_VERSION+=("${VERSION}-${BUILD_NUMBER}.${CIRCLE_BUILD_NUM}")
 fi
