a25b96609c12390200a06cf1141232a27ff8167e
Ronny Trommer <ronny@opennms.org>
2019-04-03 15:37:25 +0200

    Fix

--- projects/registry-config.sh
+++ projects/registry-config.sh
@@ -3,7 +3,7 @@
 # shellcheck disable=SC2034
 
 # Fallback set container project to working directory
-if [ -z "${CONTAINER_PROJECT}" ]; then
+if [ -n "${CONTAINER_PROJECT}" ]; then
   CONTAINER_PROJECT="$(basename "$(pwd)")"
 fi
 
