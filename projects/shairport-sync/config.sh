7413177cb1d3e960c5009b5e338372354adcdb42
Ronny Trommer <ronny@opennms.org>
2019-04-04 09:04:09 +0200

    Remove build number from shairport image

--- projects/shairport-sync/config.sh
+++ projects/shairport-sync/config.sh
@@ -9,16 +9,12 @@ BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 VERSION="3.2d22"
 
-# Allow a manual build number which allows to overwrite an existing image
-BUILD_NUMBER="b1"
-
 # Floating tags
-IMAGE_VERSION=("${VERSION}-${BUILD_NUMBER}"
-               "${VERSION}")
+IMAGE_VERSION=("${VERSION}")
 
 # Most specific tag when it is not build locally and in CircleCI
 if [ -n "${CIRCLE_BUILD_NUM}" ]; then
-  IMAGE_VERSION+=("${VERSION}-${BUILD_NUMBER}.${CIRCLE_BUILD_NUM}")
+  IMAGE_VERSION+=("${VERSION}-b${CIRCLE_BUILD_NUM}")
 fi
 
 SHAIRPORT_SYNC_URL=https://github.com/mikebrady/shairport-sync.git
