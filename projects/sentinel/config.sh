4c51cf7417b3af084e2037835e7e1b1ffc092972
Ronny Trommer <ronny@opennms.org>
2019-04-04 09:35:05 +0200

    Set openjdk 8 dependency on specific build version

--- projects/sentinel/config.sh
+++ projects/sentinel/config.sh
@@ -4,7 +4,7 @@
 
 # Base Image Dependency
 BASE_IMAGE="no42org/openjdk"
-BASE_IMAGE_VERSION="1.8.0.201.b09"
+BASE_IMAGE_VERSION="1.8.0.201.b09-b722"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # Sentinel RPM repository config and version
