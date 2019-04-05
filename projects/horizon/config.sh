aff795d67e4ba6cc0cb75363c5f62bf090bacb51
Ronny Trommer <ronny@opennms.org>
2019-04-05 20:29:12 +0200

    Migrate to OpenJDK 11

--- projects/horizon/config.sh
+++ projects/horizon/config.sh
@@ -4,7 +4,7 @@
 
 # Base Image Dependency
 BASE_IMAGE="no42org/openjdk"
-BASE_IMAGE_VERSION="1.8.0.201.b09-cb.791"
+BASE_IMAGE_VERSION="11.0.2.7-cb.792"
 BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
 
 # Horizon RPM repository config and version
