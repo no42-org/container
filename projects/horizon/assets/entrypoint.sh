fbc8f92102491ef0aef707a596cbc3e1b37dfd65
Ronny Trommer <ronny@opennms.org>
2019-04-10 18:47:00 +0200

    Add JAVA_MEM_OPTS and JAVA_OPTS

--- projects/horizon/assets/entrypoint.sh
+++ projects/horizon/assets/entrypoint.sh
@@ -78,10 +78,7 @@ start() {
   -Dgroovy.use.classvalue=true
   -Djava.io.tmpdir=/opt/opennms/data/tmp
   -XX:+StartAttachListener"
-  if [ -z "${JAVA_OPTS}" ]; then
-      JAVA_OPTS="-Xmx1G -XX:MaxMetaspaceSize=512m"
-  fi
-  exec /usr/lib/jvm/java-11/bin/java ${OPENNMS_JAVA_OPTS} ${JAVA_OPTS} -jar /opt/opennms/lib/opennms_bootstrap.jar start
+  exec /usr/lib/jvm/java-11/bin/java ${OPENNMS_JAVA_OPTS} ${JAVA_MEM_OPTS} ${JAVA_OPTS} -jar /opt/opennms/lib/opennms_bootstrap.jar start
 }
 
 testConfig() {
