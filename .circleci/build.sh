aa85fdc0b5d78387c84817cd5f6c80ab45fcdf3e
Ronny Trommer <ronny@opennms.org>
2020-06-21 21:56:48 +0200

    Add guard to build only on changes

---
+++ .circleci/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+PROJECT="$(basename "$(pwd)")"
+
+echo "Detect changes in project dir ${PROJECT}"
+if git diff --name-only HEAD^...HEAD | grep "^projects/${PROJECT}"; then
+  echo "Changes here, run the build"
+  if [ "${CIRCLE_BRANCH}" == "master" ]; then
+    echo "Publish images for master branch ..."
+    make VERSION="$(cat version.txt).${CIRCLE_BUILD_NUM}" push
+  else
+    echo "Skip publishing for working branches other than master."
+    echo "Build images for branches are available in the CircleCI build artifacts."
+    make oci
+  fi
+else
+  echo "No changes detected"
+  exit 0
+fi
